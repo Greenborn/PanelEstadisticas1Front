4712de334009295717977d87963a4704210c7fb3
berty <lucho.2012.tandil@gmail.com>
2022-01-01 09:03:11 -0300

    Avance en menu lateral, modificacion en vista home

--- src/app/modules/dashboard/components/home/home.component.ts
+++ src/app/modules/dashboard/components/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgbNav, NgbNavItem, NgbNavLink } from '@ng-bootstrap/ng-bootstrap';
+import { BootstrapSideMenuLink } from 'src/app/modules/AppUIUtils/components/bootstrap-sidemenu/model/bootstrap-sidemenu-link';
+import { BootstrapSideMenuConfig } from 'src/app/modules/AppUIUtils/components/bootstrap-sidemenu/model/bootstrap.sidemenu.config';
 import { AuthService } from 'src/app/modules/autentication/services/auth.service';
 import { DashboardService } from '../../services/dashboard.service';
 
@@ -11,7 +13,8 @@ import { DashboardService } from '../../services/dashboard.service';
 export class HomeComponent implements OnInit {
   
   public activeNav:string = "ngb-nav-0";
-  
+  public menuConfig:BootstrapSideMenuConfig = new BootstrapSideMenuConfig();
+
   constructor(
     private authService:      AuthService,
     private dashboardService: DashboardService
@@ -24,6 +27,10 @@ export class HomeComponent implements OnInit {
       this.dashboardService.paramsDetalleEquipo = response.params;
     } });
 
+    this.menuConfig.addLink(new BootstrapSideMenuLink({ id:1, icon: 'gear', name:'Equipos', color:'rgb(166, 176, 255)' }));
+    this.menuConfig.addLink(new BootstrapSideMenuLink({ id:2, icon: 'clipboard-data', name:'Estadisticas',color:'rgb(166, 176, 255)' }));
+    this.menuConfig.addLink(new BootstrapSideMenuLink({ id:3, icon: 'map', name:'Mapa', color:'rgb(166, 176, 255)' }));
+    
   }
 
   ngOnDestroy(){
